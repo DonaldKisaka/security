563805e7eded4fd92aa32a1ccc94f20927aebcf8
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-03 21:53:21 +0300

    userclass

---
+++ security/src/main/java/com/don/security/User.java
@@ -0,0 +1,24 @@
+package com.don.security;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
