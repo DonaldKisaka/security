770660d1faf0933c523cbd58fe1cb7a29c16b5a3
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-06 15:37:10 +0300

    authentication

--- security/src/main/java/com/don/security/auth/AuthenticationContoller.java
+++ security/src/main/java/com/don/security/auth/AuthenticationContoller.java
@@ -1,6 +1,7 @@
 package com.don.security.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,18 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationContoller {
 
+    private final AuthenticationService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
-        //
+        return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request
     ) {
-        //
+        return ResponseEntity.ok(service.authenticate(request))
     }
 
 
