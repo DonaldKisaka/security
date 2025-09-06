018c1bf1abddd63c9a768874eb086e395b6711ce
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-06 15:22:14 +0300

    authentication

---
+++ security/src/main/java/com/don/security/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.don.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
